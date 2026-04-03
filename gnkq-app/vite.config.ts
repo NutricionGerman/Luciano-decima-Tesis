bec11f93ee120ce4383e88ff6bb50d7b8c935520
asdefghasdefgh-sketch <asdefghasdefgh@gmail.com>
2026-04-02 22:45:03 -0300

    Update vite.config.ts

--- gnkq-app/vite.config.ts
+++ gnkq-app/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Luciano-decima-Tesis/',
+  base: '/luciano-decima-tesis/',
   plugins: [react()],
 })
